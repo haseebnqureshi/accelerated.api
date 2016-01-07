e4334e56cc6b2f0aa61faee95a25c3198ae93595
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-01-07 12:24:59 -0500

    Directives for user register and login forms

--- www/elements/elements.js
+++ www/elements/elements.js
@@ -2,16 +2,96 @@
 
 	var app = window.app;
 
-	app.directive('manageUsers', function() {
+	app.directive('formUserRegister', function() {
 		return {
 			restrict: 'E',
-			templateUrl: '/elements/manageUsers.html',
-			controller: ['$scope', '$routeParams', 'users', function($scope, $routeParams, users) {
+			templateUrl: '/elements/formUserRegister.html',
+			controller: ['$scope', '$routeParams', function($scope, $routeParams) {
 				var that = this;
-				this.users = users;
+
+				this.view = {
+					message: '',
+					messageClass: 'secondary',
+					buttonClass: 'disabled'
+				};
+
+				this.data = {
+					email: '',
+					firstName: '',
+					lastName: '',
+					password: '',
+					confirmPassword: ''
+				};
+
+				this.checkPasswordMatch = function() {
+					if (this.data.password != this.data.confirmPassword) {
+						this.view.message = 'Your passwords do not match.';
+						this.view.messageClass = 'secondary';
+						this.view.buttonClass = 'disabled';
+						return false;
+					}
+					this.view.message = 'Passwords match!';
+					this.view.messageClass = 'success';
+					this.checkFormState();
+					return true;
+				};
+
+				this.checkFormState = function() {
+					var empty = _.filter(this.data, function(value) {
+						return value === undefined || value === '';
+					});
+					this.changeButtonStatus(empty.length == 0 ? true : false);
+				};	
+
+				this.changeButtonStatus = function(statusBoolean) {
+					this.view.buttonClass = statusBoolean ? 'success' : 'disabled';
+				};
+
+				this.submit = function() {
+					if (this.checkPasswordMatch() == false) { return; }
+					
+				};
 			}],
-			controllerAs: 'ManageUsersCtrl'
+			controllerAs: 'FormUserRegisterCtrl'
 		}
 	});
+
+	app.directive('formUserLogin', function() {
+		return {
+			restrict: 'E',
+			templateUrl: '/elements/formUserLogin.html',
+			controller: ['$scope', '$routeParams', function($scope, $routeParams) {
+				var that = this;
+
+				this.view = {
+					message: '',
+					messageClass: 'secondary',
+					buttonClass: 'disabled'
+				};
+
+				this.data = {
+					email: '',
+					password: ''
+				};
+
+				this.checkFormState = function() {
+					var empty = _.filter(this.data, function(value) {
+						return value === undefined || value === '';
+					});
+					this.changeButtonStatus(empty.length == 0 ? true : false);
+				};	
+
+				this.changeButtonStatus = function(statusBoolean) {
+					this.view.buttonClass = statusBoolean ? 'success' : 'disabled';
+				};
+
+				this.submit = function() {
+
+				};
+			}],
+			controllerAs: 'FormUserLoginCtrl'
+		}
+	});
+
 
 })();
