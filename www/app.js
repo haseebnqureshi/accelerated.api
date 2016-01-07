133258afa53e249d10b829ff983a7d560e6c0517
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-01-07 14:38:27 -0500

    Have pattern ready for auth interceptor

--- www/app.js
+++ www/app.js
@@ -2,14 +2,36 @@
 
 	window.app = angular.module('appPublic', ['ngRoute']);
 	
-	window.app.config(function($routeProvider, $locationProvider) {
+	window.app.config(function($routeProvider, $locationProvider, $httpProvider) {
 		
 		$routeProvider
 			.when('/', {
 				templateUrl: '/pages/home.html'
+			})
+			.when('/dashboard', {
+				templateUrl: '/pages/dashboard.html'
 			});
 
 		$locationProvider.html5Mode(true);
+
+		$httpProvider.interceptors.push(['$location', function($location) {
+			return {
+				request: function(config) {
+					switch ($location.$$path) {
+						case '/dashboard':
+							console.log(config.headers);
+						break;
+					}
+					return config;
+				},
+				responseErr: function(response) {
+					console.log('response', response);
+					return response;
+				}
+			}
+		}]);
+
+
 	});
 
 })();
