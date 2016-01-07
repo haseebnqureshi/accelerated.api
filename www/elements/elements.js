1f0cfa1230c368a10c850176e59cfb1a1e4fc484
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-01-07 12:40:29 -0500

    Forgot password front-end directive

--- www/elements/elements.js
+++ www/elements/elements.js
@@ -49,7 +49,7 @@
 
 				this.submit = function() {
 					if (this.checkPasswordMatch() == false) { return; }
-					
+
 				};
 			}],
 			controllerAs: 'FormUserRegisterCtrl'
@@ -90,6 +90,47 @@
 				};
 			}],
 			controllerAs: 'FormUserLoginCtrl'
+		}
+	});
+
+	app.directive('formForgotPassword', function() {
+		return {
+			restrict: 'E',
+			templateUrl: '/elements/formForgotPassword.html',
+			controller: ['$scope', '$routeParams', function($scope, $routeParams) {
+				var that = this;
+
+				this.view = {
+					showFields: false,
+					message: '',
+					messageClass: 'secondary',
+					buttonClass: 'disabled'
+				};
+
+				this.data = {
+					email: ''
+				};
+
+				this.checkFormState = function() {
+					var empty = _.filter(this.data, function(value) {
+						return value === undefined || value === '';
+					});
+					this.changeButtonStatus(empty.length == 0 ? true : false);
+				};	
+
+				this.changeButtonStatus = function(statusBoolean) {
+					this.view.buttonClass = statusBoolean ? 'success' : 'disabled';
+				};
+
+				this.toggleFields = function() {
+					this.view.showFields = this.view.showFields ? false : true;
+				};
+
+				this.submit = function() {
+
+				};
+			}],
+			controllerAs: 'FormForgotPasswordCtrl'
 		}
 	});
 
