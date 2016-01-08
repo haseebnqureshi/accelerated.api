94623162e1c7a302ed119cdac0749dc9392f1e39
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-01-07 21:38:00 -0500

    Removed global-scoped ajax helper

--- www/helpers.js
+++ www/helpers.js
@@ -2,18 +2,7 @@ jQuery(function($) {
 
 	$(document).foundation();
 
-	window.ajax = function(method, resource, data, statuses) {
-		var args = {
-			url: window.endpoint + '/' + resource,
-			type: method,
-			contentType: 'application/json',
-			statusCode: statuses
-		};
-		if (data) {
-			args.data = JSON.stringify(data);
-		}
-		$.ajax(args);
-	};
+	window.endpoint = 'http://192.168.80.80:8080/v1';
 
 	window.delay = function(delay, callback) {
 		var delay = setInterval(function() {
