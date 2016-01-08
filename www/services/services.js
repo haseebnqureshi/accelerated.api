bcd63621a837c84dd98102fd68e102572cefd46e
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-01-07 21:38:36 -0500

    Created preliminary authorization service for angular

--- www/services/services.js
+++ www/services/services.js
@@ -1,20 +1,74 @@
 (function() {
 
-	window.app.factory('users', function() {
+	window.app.factory('accAuth', [function() {
 		var that = this;
-		this.getUser = function(id) {
-			alert('Service users.getUser() called');
+		this.token = {
+			header: '',
+			value: ''
 		};
-		this.putUser = function(id, args) {
-			alert('Service users.putUser() called');
+
+		this.register = function(data, success, alreadyEmail, error) {
+			$.ajax({
+				method: 'POST',
+				url: window.endpoint + '/register',
+				dataType: 'json',
+				data: {
+					email: data.email,
+					password: data.password,
+					firstName: data.firstName,
+					lastName: data.lastName
+				},
+				success: function(data, textStatus, xhr) {
+					that.login(data, success || null);
+
+					if (success) { success(); }
+				},
+				error: function(xhr) {
+					switch (xhr.status) {
+						case 400:
+							if (alreadyEmail) { alreadyEmail(); }
+						break;
+						default:
+							if (error) { error(); }
+					}
+				}
+			});
 		};
-		this.postUser = function(args) {
-			alert('Service users.postUser() called');
+
+		this.login = function(data, success, notFound, error) {
+			$.ajax({
+				method: 'POST',
+				url: window.endpoint + '/login',
+				dataType: 'json',
+				data: {
+					email: data.email,
+					password: data.password
+				},
+				success: function(data, textStatus, xhr) {
+					that.token.header = data.header;
+					that.token.value = data.value;
+
+					if (success) { success(); }
+				},
+				error: function(xhr) {
+					switch (xhr.status) {
+						case 404:
+							that.token.header = '';
+							that.token.value = '';
+							if (notFound) { notFound(); }
+						break;
+						default:
+							if (error) { error(); }
+					}
+				}
+			});
 		};
-		this.deleteUser = function(id) {
-			alert('Service users.deleteUser() called');
+
+		this.verify = function() {
+			console.log('verifying');
 		};
+
 		return this;
-	});
+	}]);
 
 })();
