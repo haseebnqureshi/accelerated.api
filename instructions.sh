4c92fdce2e3df8a25d46e53aa19c6236ae0e2a0b
Haseeb Qureshi <haseeb.n.qureshi@gmail.com>
2016-05-22 21:30:24 -0400

    Display copy-paste commands after install; bumped version number

---
+++ instructions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo ""
+echo "======================="
+echo "Accelerated Quick-Start"
+echo "======================="
+echo ""
+echo "We really put a lot of effort in making this development as fast"
+echo "as possible. So now, you can simply copy one of these two commands"
+echo "and literally copy-and-paste them in your node project's directory:"
+echo ""
+echo "a) install starter-app into 'example' directory"
+echo "cd node_modules/accelerated.api && npm run-script example ./../../ && cd ../../"
+echo ""
+echo "b) install blank template into node project directory"
+echo "cd node_modules/accelerated.api && npm run-script blank ./../../ && cd ../../"
+echo ""
+
